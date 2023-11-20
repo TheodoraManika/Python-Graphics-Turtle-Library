e4766bfaf277594f64465888957a4fde192996ba
TheodoraManika <theodoramnk@gmail.com>
2023-11-20 20:32:18 +0200

    while

---
+++ while.py
@@ -0,0 +1,19 @@
+#Περίπτωση 1
+num = 5
+
+while num < 10:
+    print(num)
+    num += 1
+
+#Περίπτωση 2
+secret_name = "theodora"
+name = "a"
+
+while secret_name != name:  # != σημαίνει διάφορο
+    name = input("Find my name: ")
+
+print(name + " You found it")
+
+#Περίπτωση 3
+while True:
+    print("hi")
