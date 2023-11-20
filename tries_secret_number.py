7f7c5b2d8919f3178b01648bdb65c52059360a2c
TheodoraManika <theodoramnk@gmail.com>
2023-11-20 21:04:29 +0200

    while_secret_number

--- tries_secret_number.py
+++ tries_secret_number.py
@@ -1,12 +1,16 @@
 import random
 secret = random.randrange(1, 101) 
 guess = 0
+tries = 0 
 
 while guess != secret:
     guess = int(input("Μαντεψε έναν αριθμό από το 1 ως 100: "))
+    tries += 1
     if guess > secret:
         print("Πολύ μεγάλος!")
     elif guess < secret:
         print("Πολύ μικρός!")
     else:
         print("To βρήκες!")
+
+print("You tried " + str(tries) + " times")
