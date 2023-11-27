91e9940a5661a9efb1675eb33684b572467de6fa
TheodoraManika <theodoramnk@gmail.com>
2023-11-27 21:08:40 +0200

    max_list

--- list_max.py
+++ list_max.py
@@ -6,3 +6,10 @@ for i in range(51):
     numbers.append(num)
 
 print(numbers)
+
+max = numbers[0]
+for i in range(1, len(numbers)):
+    if max < numbers[i]:
+        max = numbers[i]
+
+print(max, "is the  greatest")
